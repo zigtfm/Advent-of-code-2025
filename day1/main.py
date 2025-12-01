0a26c041d360fe90d8a89f4220e81aa02dda945a
zigtfm <52834004+zigtfm@users.noreply.github.com>
2025-12-01 21:17:32 +0500

    optimized

--- day1/main.py
+++ day1/main.py
@@ -21,30 +21,12 @@ input.close()
 dial = 50
 count = 0
 
-debug_dial = 50
-debug_count = 0
-
 for x in instructions:
-    #region Check
-    debug_x = x
-    debug_dial_pre = dial
-
-    sign = 1 if x > 0 else -1
-
-    for i in range(0, x, sign):
-        debug_dial = (debug_dial + sign) % 100
-
-        if debug_dial == 0:
-            debug_count += 1
-
-    #endregion
-    debug_add_count_a = 0
+    dial_pre = dial
 
     abs_x = abs(x)
     if abs_x >= 100:
         count += abs_x // 100
-
-        debug_add_count_a = abs_x // 100
 
         if x > 0:
             x = x % 100
@@ -52,26 +34,12 @@ for x in instructions:
             x = x % -100
 
     dial += x
-    debug_add_count_b = 0
 
-
-    if debug_dial_pre != 0:
-        if (dial >= 100) or (dial <= 0):
-            count += 1
-            debug_add_count_b = 1
+    if dial_pre != 0 and ((dial >= 100) or (dial <= 0)):
+        count += 1
 
     dial = dial % 100
 
-
-    #region Check
-
-    if debug_count != count:
-        print("breakpoint")
-
-    #endregion
-
 print(count)
-
-#6858
 
 #endregion
