b3bd19ca239054a56afdd36efbe6b59c19a82702
zigtfm <52834004+zigtfm@users.noreply.github.com>
2025-12-02 15:08:51 +0500

    cleanup

--- day2/main.py
+++ day2/main.py
@@ -15,41 +15,28 @@ input.close()
 #endregion
 
 
-#region Solution Part 1
+#region Solution
 
 import re
 
-sum = 0
+def solution_re(pattern: re.Pattern) -> int:
+    sum = 0
 
-for x in instructions:
-    for i in range(x[0], x[1]+1):
-        str_i = str(i)
+    for x in instructions:
+        for i in range(x[0], x[1]+1):
+            str_i = str(i)
 
-        result = re.fullmatch(r'(.+)\1', str_i)
-        if result != None:
-            if result.string == str_i:
-                sum += i
+            result = re.fullmatch(pattern, str_i)
+            if result != None:
+                if result.string == str_i:
+                    sum += i
 
-print(sum)
-
-#endregion
-
-#region Solution Part 2
-
-import re
-
-sum = 0
-
-for x in instructions:
-    for i in range(x[0], x[1]+1):
-        str_i = str(i)
-
-        result = re.fullmatch(r'(.+)(\1)+', str_i)
-        if result != None:
-            if result.string == str_i:
-                sum += i
+    return sum
 
 
-print(sum)
+# Slow solution
+print(solution_re(r'(.+)\1'))
+print(solution_re(r'(.+)(\1)+'))
+
 
 #endregion
